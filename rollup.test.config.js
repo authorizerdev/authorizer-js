414e7686b2587c03e10b2d9a17b8f38ec9b4508f
productdevbook <hi@productdevbook.com>
2023-03-03 08:56:19 +0300

    refactory fixed all code eslint

--- rollup.test.config.js
+++ rollup.test.config.js
@@ -1,23 +1,23 @@
-import typescript from '@rollup/plugin-typescript';
-import serve from 'rollup-plugin-serve';
+import typescript from '@rollup/plugin-typescript'
+import serve from 'rollup-plugin-serve'
 
 export default {
-	input: 'src/index.ts',
-	watch: {
-		include: 'src/*',
-	},
-	plugins: [
-		typescript(),
-		serve({
-			contentBase: '__test__',
-			verbose: true,
-		}),
-	],
-	output: {
-		file: `__test__/lib/authorizer.js`,
-		format: 'umd',
-		name: 'authorizerdev', // this is the name of the global object
-		esModule: false,
-		sourcemap: true,
-	},
-};
+  input: 'src/index.ts',
+  watch: {
+    include: 'src/*',
+  },
+  plugins: [
+    typescript(),
+    serve({
+      contentBase: '__test__',
+      verbose: true,
+    }),
+  ],
+  output: {
+    file: '__test__/lib/authorizer.js',
+    format: 'umd',
+    name: 'authorizerdev', // this is the name of the global object
+    esModule: false,
+    sourcemap: true,
+  },
+}
