79ec3a86ff853f54df019f0a02e922e27ec18987
Lakhan Samani <lakhan.m.samani@gmail.com>
2023-04-15 13:10:26 +0530

    [chore]: fix lint

--- src/types.ts
+++ src/types.ts
@@ -1,212 +1,212 @@
 export interface ConfigType {
-  authorizerURL: string;
-  redirectURL: string;
-  clientID: string;
-  extraHeaders?: Record<string, string>;
+  authorizerURL: string
+  redirectURL: string
+  clientID: string
+  extraHeaders?: Record<string, string>
 }
 
 export interface User {
-  id: string;
-  email: string;
-  preferred_username: string;
-  email_verified: boolean;
-  signup_methods: string;
-  given_name?: string | null;
-  family_name?: string | null;
-  middle_name?: string | null;
-  nickname?: string | null;
-  picture?: string | null;
-  gender?: string | null;
-  birthdate?: string | null;
-  phone_number?: string | null;
-  phone_number_verified?: boolean | null;
-  roles?: string[];
-  created_at: number;
-  updated_at: number;
-  is_multi_factor_auth_enabled?: boolean;
+  id: string
+  email: string
+  preferred_username: string
+  email_verified: boolean
+  signup_methods: string
+  given_name?: string | null
+  family_name?: string | null
+  middle_name?: string | null
+  nickname?: string | null
+  picture?: string | null
+  gender?: string | null
+  birthdate?: string | null
+  phone_number?: string | null
+  phone_number_verified?: boolean | null
+  roles?: string[]
+  created_at: number
+  updated_at: number
+  is_multi_factor_auth_enabled?: boolean
 }
 
 export interface AuthToken {
-  message?: string;
-  access_token: string;
-  expires_in: number;
-  id_token: string;
-  refresh_token?: string;
-  user?: User;
-  should_show_otp_screen?: boolean;
+  message?: string
+  access_token: string
+  expires_in: number
+  id_token: string
+  refresh_token?: string
+  user?: User
+  should_show_otp_screen?: boolean
 }
 
 export interface Response {
-  message: string;
+  message: string
 }
 
-export type Headers = Record<string, string>;
+export type Headers = Record<string, string>
 
 export interface LoginInput {
-  email: string;
-  password: string;
-  roles?: string[];
-  scope?: string[];
-  state?: string;
+  email: string
+  password: string
+  roles?: string[]
+  scope?: string[]
+  state?: string
 }
 
 export interface SignupInput {
-  email: string;
-  password: string;
-  confirm_password: string;
-  given_name?: string;
-  family_name?: string;
-  middle_name?: string;
-  nickname?: string;
-  picture?: string;
-  gender?: string;
-  birthdate?: string;
-  phone_number?: string;
-  roles?: string[];
-  scope?: string[];
-  redirect_uri?: string;
-  is_multi_factor_auth_enabled?: boolean;
-  state?: string;
+  email: string
+  password: string
+  confirm_password: string
+  given_name?: string
+  family_name?: string
+  middle_name?: string
+  nickname?: string
+  picture?: string
+  gender?: string
+  birthdate?: string
+  phone_number?: string
+  roles?: string[]
+  scope?: string[]
+  redirect_uri?: string
+  is_multi_factor_auth_enabled?: boolean
+  state?: string
 }
 
 export interface MagicLinkLoginInput {
-  email: string;
-  roles?: string[];
-  scopes?: string[];
-  state?: string;
-  redirect_uri?: string;
+  email: string
+  roles?: string[]
+  scopes?: string[]
+  state?: string
+  redirect_uri?: string
 }
 
 export interface VerifyEmailInput {
-  token: string;
-  state?: string;
+  token: string
+  state?: string
 }
 
 export interface VerifyOtpInput {
-  email: string;
-  otp: string;
-  state?: string;
+  email: string
+  otp: string
+  state?: string
 }
 
 export interface ResendOtpInput {
-  email: string;
+  email: string
 }
 
 export interface GraphqlQueryInput {
-  query: string;
-  variables?: Record<string, any>;
-  headers?: Headers;
+  query: string
+  variables?: Record<string, any>
+  headers?: Headers
 }
 
 export interface MetaData {
-  version: string;
-  client_id: string;
-  is_google_login_enabled: boolean;
-  is_facebook_login_enabled: boolean;
-  is_github_login_enabled: boolean;
-  is_linkedin_login_enabled: boolean;
-  is_apple_login_enabled: boolean;
-  is_twitter_login_enabled: boolean;
-  is_microsoft_login_enabled: boolean;
-  is_email_verification_enabled: boolean;
-  is_basic_authentication_enabled: boolean;
-  is_magic_link_login_enabled: boolean;
-  is_sign_up_enabled: boolean;
-  is_strong_password_enabled: boolean;
+  version: string
+  client_id: string
+  is_google_login_enabled: boolean
+  is_facebook_login_enabled: boolean
+  is_github_login_enabled: boolean
+  is_linkedin_login_enabled: boolean
+  is_apple_login_enabled: boolean
+  is_twitter_login_enabled: boolean
+  is_microsoft_login_enabled: boolean
+  is_email_verification_enabled: boolean
+  is_basic_authentication_enabled: boolean
+  is_magic_link_login_enabled: boolean
+  is_sign_up_enabled: boolean
+  is_strong_password_enabled: boolean
 }
 
 export interface UpdateProfileInput {
-  old_password?: string;
-  new_password?: string;
-  confirm_new_password?: string;
-  email?: string;
-  given_name?: string;
-  family_name?: string;
-  middle_name?: string;
-  nickname?: string;
-  gender?: string;
-  birthdate?: string;
-  phone_number?: string;
-  picture?: string;
-  is_multi_factor_auth_enabled?: boolean;
+  old_password?: string
+  new_password?: string
+  confirm_new_password?: string
+  email?: string
+  given_name?: string
+  family_name?: string
+  middle_name?: string
+  nickname?: string
+  gender?: string
+  birthdate?: string
+  phone_number?: string
+  picture?: string
+  is_multi_factor_auth_enabled?: boolean
 }
 
 export interface ForgotPasswordInput {
-  email: string;
-  state?: string;
-  redirect_uri?: string;
+  email: string
+  state?: string
+  redirect_uri?: string
 }
 
 export interface ResetPasswordInput {
-  token: string;
-  password: string;
-  confirm_password: string;
+  token: string
+  password: string
+  confirm_password: string
 }
 
 export interface SessionQueryInput {
-  roles?: string[];
+  roles?: string[]
 }
 
 export interface IsValidJWTQueryInput {
-  jwt: string;
-  roles?: string[];
+  jwt: string
+  roles?: string[]
 }
 
 export interface ValidJWTResponse {
-  valid: string;
-  message: string;
+  valid: string
+  message: string
 }
 
 export enum OAuthProviders {
-  Apple = "apple",
-  Github = "github",
-  Google = "google",
-  Facebook = "facebook",
-  LinkedIn = "linkedin",
+  Apple = 'apple',
+  Github = 'github',
+  Google = 'google',
+  Facebook = 'facebook',
+  LinkedIn = 'linkedin',
 }
 
 export enum ResponseTypes {
-  Code = "code",
-  Token = "token",
+  Code = 'code',
+  Token = 'token',
 }
 
 export interface AuthorizeInput {
-  response_type: ResponseTypes;
-  use_refresh_token?: boolean;
-  response_mode?: string;
+  response_type: ResponseTypes
+  use_refresh_token?: boolean
+  response_mode?: string
 }
 
 export interface AuthorizeResponse {
-  state: string;
-  code?: string;
-  error?: string;
-  error_description?: string;
+  state: string
+  code?: string
+  error?: string
+  error_description?: string
 }
 
 export interface RevokeTokenInput {
-  refresh_token: string;
+  refresh_token: string
 }
 
 export interface GetTokenInput {
-  code?: string;
-  grant_type?: string;
-  refresh_token?: string;
+  code?: string
+  grant_type?: string
+  refresh_token?: string
 }
 
 export interface GetTokenResponse {
-  access_token: string;
-  expires_in: number;
-  id_token: string;
-  refresh_token?: string;
+  access_token: string
+  expires_in: number
+  id_token: string
+  refresh_token?: string
 }
 
 export interface ValidateJWTTokenInput {
-  token_type: "access_token" | "id_token" | "refresh_token";
-  token: string;
-  roles?: string[];
+  token_type: 'access_token' | 'id_token' | 'refresh_token'
+  token: string
+  roles?: string[]
 }
 
 export interface ValidateJWTTokenResponse {
-  is_valid: boolean;
-  claims: Record<string, any>;
+  is_valid: boolean
+  claims: Record<string, any>
 }
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,40 +1,40 @@
-import { defineConfig } from "tsup";
-import pkg from "./package.json";
-const external = [...Object.keys(pkg.dependencies || {})];
+import { defineConfig } from 'tsup'
+import pkg from './package.json'
+const external = [...Object.keys(pkg.dependencies || {})]
 
 export default defineConfig(() => [
   {
-    entryPoints: ["src/index.ts"],
-    outDir: "lib",
-    target: "node16",
-    format: ["esm", "cjs"],
+    entryPoints: ['src/index.ts'],
+    outDir: 'lib',
+    target: 'node16',
+    format: ['esm', 'cjs'],
     clean: true,
     dts: true,
     minify: true,
     external,
   },
   {
-    entry: { bundle: "src/index.ts" },
-    outDir: "lib",
-    format: ["iife"],
-    globalName: "__AUTHORIZER_JS__",
+    entry: { bundle: 'src/index.ts' },
+    outDir: 'lib',
+    format: ['iife'],
+    globalName: '__AUTHORIZER_JS__',
     clean: false,
     minify: true,
-    platform: "browser",
+    platform: 'browser',
     dts: false,
-    name: "authorizer",
+    name: 'authorizer',
     // esbuild `globalName` option generates `var __AUTHORIZER_JS__ = (() => {})()`
     // and var is not guaranteed to assign to the global `window` object so we make sure to assign it
     footer: {
-      js: "window.__TAURI__ = __AUTHORIZER_JS__",
+      js: 'window.__TAURI__ = __AUTHORIZER_JS__',
     },
     outExtension({ format, options }) {
       return {
-        js: ".min.js",
-      };
+        js: '.min.js',
+      }
     },
     esbuildOptions(options, ctx) {
-      options.entryNames = "authorizer";
+      options.entryNames = 'authorizer'
     },
   },
-]);
+])
